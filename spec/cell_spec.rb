e932d5d9124daf15c203351634f332caa177b7d8
supersakana <zacwilliamson@icloud.com>
2022-05-27 14:29:55 -0600

    added tests for #create_piece method

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,42 +3,56 @@
 # rspec spec/cell_spec.rb
 # rspec --format documentation spec/cell_spec.rb
 
-# rubocop:disable Metrics/BlockLength
 require_relative '../lib/cell'
 
 describe Cell do
   subject(:cell) { described_class.new([0, 0]) }
+  let(:rook) { double('rook') }
+  before do
+    cell.instance_variable_get(:@piece)
+  end
   describe '#create_piece' do
     context 'when an start position is input' do
       before do
         allow(cell).to receive(:set_color).and_return(:light_white)
       end
       it 'returns a Rook' do
-        cell.instance_variable_set(:@value, [0, 0])
-        let(:mock_rook) { instance_double(Rook) }
-        expect()
+        value = [0, 0]
+        cell.create_piece(value)
+        expect(cell.piece).to be_a(Rook)
       end
       it 'returns a Bishop' do
-        # test to run
+        value = [0, 5]
+        cell.create_piece(value)
+        expect(cell.piece).to be_a(Bishop)
       end
       it 'returns a Knight' do
-        # test to run
+        value = [7, 1]
+        cell.create_piece(value)
+        expect(cell.piece).to be_a(Knight)
       end
       it 'returns a King' do
-        # test to run
+        value = [7, 4]
+        cell.create_piece(value)
+        expect(cell.piece).to be_a(King)
       end
       it 'returns a Queen' do
-        # test to run
+        value = [0, 3]
+        cell.create_piece(value)
+        expect(cell.piece).to be_a(Queen)
       end
       it 'returns a Pawn' do
-        # testto run
+        value = [6, 1]
+        cell.create_piece(value)
+        expect(cell.piece).to be_a(Pawn)
       end
     end
     context 'when a vaccant position is input' do
       it 'returns a nil position' do
-        # test to run
+        value = [5, 0]
+        cell.create_piece(value)
+        expect(cell.piece).to eq(nil)
       end
     end
   end
 end
-# rubocop:enable Metrics/BlockLength

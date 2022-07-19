b674cedb9286a4f6785220685f36116ae0034903
supersakana <zacwilliamson@icloud.com>
2022-07-19 12:47:33 -0600

    refactored pawn file

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop:disable Style/EmptyCaseCondition, Layout/MultilineOperationIndentation
+
 # contains pawn functionality
 class Pawn < Piece
   attr_accessor :pawn_jump, :jumped, :ep_enabled
@@ -17,7 +19,6 @@ class Pawn < Piece
   end
 
   # list of increments needed to find possible pawn moves
-  # rubocop:disable Style/EmptyCaseCondition
   def transitions
     case
     when @color == :light_white && @jump_enabled == true then pawn_shifts[:white_jump]
@@ -27,7 +28,6 @@ class Pawn < Piece
       pawn_shifts[:black]
     end
   end
-  # rubocop:enable Style/EmptyCaseCondition
 
   # list of pawn transitions
   def pawn_shifts
@@ -40,9 +40,8 @@ class Pawn < Piece
   # returns legal moves given pawn transitions
   def iterate(shift, start, board)
     @ep_enabled = true if ep_true?(start, board)
-
     move = create_move(shift, start)
-    if pawn_conditions?(shift, start, move, board)
+    if conditions_met?(shift, move, start, board)
       [move]
     else
       []
@@ -68,28 +67,28 @@ class Pawn < Piece
   private
 
   # returns true if all pawn conditions are met
-  def pawn_conditions?(shift, start, move, board)
-    (vertical_shift?(shift, move, board) && blocked_jump?(shift, move, board)) ||
-      (diagonal_shift?(shift) && opposing_piece?(move, start, board)) ||
-      ep_open?(start, move, board, @ep_enabled)
+  def conditions_met?(shift, move, start, board)
+    verti?(shift, move, board) ||
+    diag?(shift, move, start, board) ||
+    ep_open?(start, move, board, @ep_enabled)
   end
 
-  # returns true if pawn move has a verticle opening
-  def vertical_shift?(shift, move, board)
-    board.cells[move].empty? && shift.include?(0)
+  # returns true if valid verticle move
+  def verti?(shift, move, board)
+    board.cells[move].empty? && shift.include?(0) && can_jump?(shift, move, board)
   end
 
-  # returns true if pawn transition is diagonal
-  def diagonal_shift?(shift)
-    [[1, 1], [-1, 1], [1, -1], [-1, -1]].include?(shift)
+  # returns true if valid diagonal move
+  def diag?(shift, move, start, board)
+    [[1, 1], [-1, 1], [1, -1], [-1, -1]].include?(shift) && opposing_piece?(move, start, board)
   end
 
   # checks if a user is blocking a pawn jump
-  def blocked_jump?(shift, move, board)
+  def can_jump?(shift, move, board)
     return true unless [2, -2].include?(shift[1])
 
     prev = shift == [0, 2] ? [move[0], move[1] - 1] : [move[0], move[1] + 1]
-
     board.cells[prev].empty?
   end
 end
+# rubocop:enable Style/EmptyCaseCondition, Layout/MultilineOperationIndentation

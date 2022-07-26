3d0600bd54b9ef32335d4d00f63f35b6fe5a911b
supersakana <zacwilliamson@icloud.com>
2022-07-26 09:05:28 -0600

    refactored methods in promotion module

--- lib/promotion.rb
+++ lib/promotion.rb
@@ -9,40 +9,41 @@ module Promotion
   include Display
   # converts a pawn to promo piece if pawn can promote
   def promote(board, land)
-    return unless promote?(board, land)
+    pawn = board.cells[land]
+    return unless promote?(pawn)
 
     promo = display_promotion
-    validate_promo(board, land, promo)
+    validate_promo(board, land, pawn, promo)
   end
 
   # returns true if a pawn piece need promotion
-  def promote?(board, land)
-    return unless board.cells[land].pawn?
+  def promote?(pawn)
+    return unless pawn.pawn?
 
-    board.cells[land].piece_color == :light_white && board.cells[land].value[1] == 7 ||
-    board.cells[land].piece_color == :black && board.cells[land].value[1].zero?
+    pawn.piece_color == :light_white && pawn.value[1] == 7 ||
+    pawn.piece_color == :black && pawn.value[1].zero?
   end
 
   # converts pawn into inputted promo, else repromts user
-  def validate_promo(board, land, promo)
+  def validate_promo(board, land, pawn, promo)
     if %w[r b k q].include?(promo)
-      convert(board, land, promo)
+      convert(pawn, promo)
     else
-      promote(land)
+      promote(board, land)
     end
   end
 
   # converts a piece from piece to promotion piece
-  def convert(board, land, promo)
-    color = board.cells[land].piece_color == :black ? [7, 7] : [0, 0]
+  def convert(pawn, promo)
+    color = pawn.piece_color == :black ? [7, 7] : [0, 0]
 
-    board.cells[land].piece = case promo
-                              when 'r' then Rook.new(color)
-                              when 'b' then Bishop.new(color)
-                              when 'k' then Knight.new(color)
-                              else
-                                Queen.new(color)
-                              end
+    pawn.piece = case promo
+                 when 'r' then Rook.new(color)
+                 when 'b' then Bishop.new(color)
+                 when 'k' then Knight.new(color)
+                 else
+                   Queen.new(color)
+                 end
   end
 end
 # rubocop:enable Layout/MultilineOperationIndentation
